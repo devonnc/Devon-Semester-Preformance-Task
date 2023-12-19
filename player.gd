4915c5cef8b23073215edd094c773fa30f1631c2
devonnc <121971591+devonnc@users.noreply.github.com>
2023-12-19 12:09:37 -0800

    Player scene complete

---
+++ player.gd
@@ -0,0 +1,19 @@
+extends Area2D
+@export var Speed = 400
+
+
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_down"):
+		velocity += 1
+	if Input.is_action_pressed("move_left"):
+		velocity -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity += 1
+	if Input.is_action_pressed("move_up"):
+		velocity -= 1
+	
+	if velocity.length() > 0:
+		velocity = velocity * Speed
+	position += velocity * delta
+
